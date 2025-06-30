397fe5f475211a39043845287fe592e1e4a44a0d
easterncc001 <prestonlee847@gmail.com>
2025-06-30 13:36:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -33,9 +33,131 @@ class Break(db.Model):
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime)
 
-@app.route("/")
+JOB_SITES = [
+    "2025 DC water - Washington DC 20032",
+    "BRWRF Phase 3 Package 1 - Ashburn 20147",
+    "DC Water Projects - Washington DC 20032",
+    "[HMMA] Stamp Shop Roof Improvement - Montgomery, Alabama 36105",
+    "Hyundai Product Center Office - Carnesville, Georgia",
+    "JWA22 - Kokomo, Indiana 46901",
+    "[KARR] Korean Ambassador's Residence Renovation, NW Washington 20016",
+    "LGES-ALPHA Project-1F0 - Queen Creek 85140",
+    "LGESMI2 EV Battery Plant Main BLDG - Holland Michigan 49423",
+    "TPO Roof Project - Fairfax 22030"
+]
+
+def generate_code():
+    import random
+    return str(random.randint(100000, 999999))
+
+def format_seconds(secs):
+    hours = int(secs // 3600)
+    minutes = int((secs % 3600) // 60)
+    return f"{hours}h {minutes}m"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        action = request.form.get("action")
+        now = datetime.now()
+
+        if action == "clockin":
+            name = request.form.get("name", "").strip()
+            job_site = request.form.get("job_site", "")
+            if not name:
+                flash("Please enter your name.", "error")
+                return redirect(url_for("index"))
+            if not job_site:
+                flash("Please select a job site.", "error")
+                return redirect(url_for("index"))
+            code = generate_code()
+            shift = Shift(name=name, job_site=job_site, clock_in=now, code=code)
+            db.session.add(shift)
+            db.session.commit()
+            flash(
+                f"Your code is: <b>{code}</b><br>"
+                f"<span style='color:red;'>This code is required to clock out. Please write it down or remember it. It will not be shown again!</span>",
+                "success"
+            )
+            return redirect(url_for("index"))
+
+        elif action == "break":
+            input_code = request.form.get("input_code")
+            if not input_code:
+                flash("Please enter your code to start a break.", "error")
+                return redirect(url_for("index"))
+            shift = Shift.query.filter_by(code=input_code).first()
+            if not shift:
+                flash("Code not found.", "error")
+                return redirect(url_for("index"))
+            last_break = Break.query.filter_by(shift_code=input_code).order_by(Break.id.desc()).first()
+            if not last_break or last_break.end is not None:
+                new_break = Break(shift_code=input_code, start=now)
+                db.session.add(new_break)
+                db.session.commit()
+                flash("Break started.", "success")
+            else:
+                flash("You are already on a break.", "error")
+            return redirect(url_for("index"))
+
+        elif action == "resume":
+            input_code = request.form.get("input_code")
+            if not input_code:
+                flash("Please enter your code to resume.", "error")
+                return redirect(url_for("index"))
+            last_break = Break.query.filter_by(shift_code=input_code).order_by(Break.id.desc()).first()
+            if last_break and last_break.end is None:
+                last_break.end = now
+                db.session.commit()
+                flash("Break ended.", "success")
+            else:
+                flash("No break to resume.", "error")
+            return redirect(url_for("index"))
+
+        elif action == "clockout":
+            input_code = request.form.get("input_code")
+            if not input_code:
+                flash("Please enter your code to clock out.", "error")
+                return redirect(url_for("index"))
+            shift = Shift.query.filter_by(code=input_code).first()
+            if not shift:
+                flash("Code not found. Please check your code.", "error")
+                return redirect(url_for("index"))
+            if shift.clock_out:
+                flash("You have already clocked out for this shift.", "error")
+                return redirect(url_for("index"))
+            clock_in_dt = shift.clock_in
+            clock_out_dt = now
+            total_time = clock_out_dt - clock_in_dt
+            breaks = Break.query.filter_by(shift_code=input_code).all()
+            total_break = 0
+            breaks_str = ""
+            if breaks:
+                for b in breaks:
+                    if b.start and b.end:
+                        total_break += (b.end - b.start).total_seconds()
+                breaks_str = "; ".join(
+                    f"{b.start.strftime('%I:%M %p')} - {b.end.strftime('%I:%M %p')}" for b in breaks if b.start and b.end
+                )
+            working_time = total_time.total_seconds() - total_break
+            shift.clock_out = clock_out_dt
+            shift.total_time = format_seconds(total_time.total_seconds())
+            shift.working_time = format_seconds(working_time)
+            shift.breaks = breaks_str
+            db.session.commit()
+            flash(
+                f"Shift complete!<br>"
+                f"Total time: <b>{format_seconds(total_time.total_seconds())}</b><br>"
+                f"Actual working time: <b>{format_seconds(working_time)}</b>",
+                "success"
+            )
+            return redirect(url_for("index"))
+
+        else:
+            flash("Invalid action or state.", "error")
+            return redirect(url_for("index"))
+
+    return render_template("index.html", job_sites=JOB_SITES)
 
 @app.route("/admin")
 def admin_view():
