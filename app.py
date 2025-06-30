038d8c9ae256fd906bf68b40a4ed156250de5e87
easterncc001 <prestonlee847@gmail.com>
2025-06-30 11:01:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -230,5 +230,7 @@ def download_excel():
     return render_template("download_excel.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
