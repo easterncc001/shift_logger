73739e021131704152770272e14304e41da9e626
easterncc001 <prestonlee847@gmail.com>
2025-06-30 08:46:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -214,4 +214,5 @@ def download_excel():
     return send_file(EXCEL_FILE, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
